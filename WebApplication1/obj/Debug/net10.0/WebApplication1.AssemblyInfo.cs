4aff88db0c80b7150e752463efc5aa6c3b36bfe1
TrueBuch <theduak.dev@gmail.com>
2026-01-22 06:34:46 +0500

    fix

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32c4020ac4da2cb2694ba5964e19cac5d24a1e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0c922d4727d4d3c0de924b68de7b25bb178adfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
